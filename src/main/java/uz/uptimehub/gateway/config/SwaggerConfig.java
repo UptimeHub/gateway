ae8c9748277dcbe85be5c0b3e17878dd79b4bd5a
xushnudbek-prog <xushnudbek756@gmail.com>
2026-05-11 19:03:09 +0500

    Integrate Keycloak OAuth2 configuration and update Swagger and application settings

--- src/main/java/uz/uptimehub/gateway/config/SwaggerConfig.java
+++ src/main/java/uz/uptimehub/gateway/config/SwaggerConfig.java
@@ -3,6 +3,9 @@ package uz.uptimehub.gateway.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
@@ -11,14 +14,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @OpenAPIDefinition(
         info = @Info(title = "UptimeHub API", version = "1.0"),
-        servers = @Server(url = "http://localhost:8080", description = "Local Server"),
-        security = @SecurityRequirement(name = "Bearer Authentication")
+        servers = @Server(url = "http://localhost:8899", description = "Local Server"),
+        security = @SecurityRequirement(name = "Keycloak")
 )
 @SecurityScheme(
-        name = "Bearer Authentication",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer"
+        name = "Keycloak",
+        type = SecuritySchemeType.OAUTH2,
+        flows = @OAuthFlows(
+                authorizationCode = @OAuthFlow(
+                        authorizationUrl = "http://localhost:8080/realms/booking-system/protocol/openid-connect/auth",
+                        tokenUrl = "http://localhost:8080/realms/booking-system/protocol/openid-connect/token",
+                        scopes = {
+                                @OAuthScope(name = "openid", description = "OpenID Connect scope"),
+                                @OAuthScope(name = "profile", description = "User profile"),
+                                @OAuthScope(name = "email", description = "User email")
+                        }
+                )
+        )
 )
 public class SwaggerConfig {
 }
